48180fda3fe83702816acd824c909103bdb6e3c0
DiogoTakayama <diogo.takayamas@gmail.com>
2017-02-15 17:43:22 -0300

    Create 1005.c

---
+++ 1005.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main (){
+double A,B;
+printf("");
+scanf("%lf",&A);
+printf("");
+scanf("%lf",&B);
+printf("MEDIA = %0.5lf\n",(A*3.5+B*7.5)/11);
+return 0;
+}
