44aee02098195704e7d82b7144f63bde94a873df
DiogoTakayama <diogo.takayamas@gmail.com>
2017-02-15 22:54:43 -0300

    Create 1006.c

---
+++ 1006.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main (){
+    double A,B,C;
+double MEDIA;
+printf("");
+scanf("%lf",&A);
+printf("");
+scanf("%lf",&B);
+printf("");
+scanf("%lf",&C);
+MEDIA = (A * 2 + B * 3 + C*5)/10.0 ;
+printf("MEDIA = %0.1lf\n",MEDIA);
+return 0;
+}
