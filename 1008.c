cd8de10c56841762b548e7b68cde34caaa23e47a
DiogoTakayama <diogo.takayamas@gmail.com>
2017-02-22 21:24:33 -0300

    Add files via upload

---
+++ 1008.c
@@ -0,0 +1,23 @@
+	#include <stdio.h>
+	int main () {
+	int numfun;
+	double  horastrabalhadas;
+	double valorhorastrabalhadas;
+
+
+	scanf ("%d",&numfun);
+
+	scanf ("%lf",&horastrabalhadas);
+	scanf ("%lf",&valorhorastrabalhadas);
+	
+	printf("NUMBER = %d ",numfun);
+	printf("SALARY = %.2lf",horastrabalhadas*valorhorastrabalhadas);
+	
+	return 0;
+
+}
+
+
+
+
+
