8154ee65f5d7b945a70d4d29e959449d69496026
DiogoTakayama <diogo.takayamas@gmail.com>
2017-03-07 11:49:55 -0300

    Add files via upload

---
+++ 1038.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main (){
+
+int codigo, quantidade;
+scanf ("%d%d",&codigo,&quantidade);
+
+if ( codigo == 1 ){
+    printf ("Total: R$ %.2lf\n",4.00*quantidade);
+
+}
+if ( codigo == 2 ){
+    printf ("Total: R$ %.2lf\n",4.50*quantidade);
+
+}
+if ( codigo == 3 ){
+    printf ("Total: R$ %.2lf\n",5.00*quantidade);
+
+}
+if ( codigo == 4 ){
+    printf ("Total: R$ %.2lf\n",2.00*quantidade);
+
+}
+if ( codigo == 5 ){
+    printf ("Total: R$ %.2lf\n",1.50*quantidade);
+
+}
+
+
+return 0;
+
+}
