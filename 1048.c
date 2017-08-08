64fd60a169826b5697b11170cf0e30af9771a4c5
DiogoTakayama <diogo.takayamas@gmail.com>
2017-08-08 17:07:47 -0300

    Update 1048.c

--- 1048.c
+++ 1048.c
@@ -3,7 +3,7 @@
 int main () {
 
 float sal;
-
+const char porcentagem = '%';
 scanf ("%f",&sal);
 
 if (sal >=0.0 && sal <= 400.00){
@@ -12,7 +12,7 @@ if (sal >=0.0 && sal <= 400.00){
 
 printf ("Novo salario: %.2f\n",sal + (sal * 15/100.00));
 printf ("Reajuste ganho: %.2f\n",sal * 15/100.00);
-printf ("Em percentual:  %i %%\n",15);
+printf ("Em percentual: 15 %c\n",porcentagem);
 
 }
 
@@ -22,7 +22,7 @@ else if (sal >=400.01 && sal <= 800.00){
 
 printf ("Novo salario: %.2f\n",sal + (sal * 12/100.00));
 printf ("Reajuste ganho: %.2f\n",sal * 12/100.00);
-printf ("Em percentual:  %i %%\n",12);
+printf ("Em percentual: 12 %c\n",porcentagem);
 
 }
 else if (sal >=800.01 && sal <= 1200.00){
@@ -31,7 +31,7 @@ else if (sal >=800.01 && sal <= 1200.00){
 
 printf ("Novo salario: %.2f\n",sal + (sal * 10/100.00));
 printf ("Reajuste ganho: %.2f\n",sal * 10/100.00);
-printf ("Em percentual:  %i %%\n",10);
+printf ("Em percentual: 10 %c\n",porcentagem);
 
 }
 else if (sal >=1200.01 && sal <= 2000.00){
@@ -40,7 +40,7 @@ else if (sal >=1200.01 && sal <= 2000.00){
 
 printf ("Novo salario: %.2f\n",sal + (sal * 7/100.00));
 printf ("Reajuste ganho: %.2f\n",sal * 7/100.00);
-printf ("Em percentual:  %i %%\n",7);
+printf ("Em percentual: 7 %c\n",porcentagem);
 
 }
 else if (sal > 2000.00){
@@ -49,7 +49,7 @@ else if (sal > 2000.00){
 
 printf ("Novo salario: %.2f\n",sal + (sal * 4/100.00));
 printf ("Reajuste ganho: %.2f\n",sal * 4/100.00);
-printf ("Em percentual:  %i %%\n",4);
+printf ("Em percentual: 4 %c\n",porcentagem);
 
 }
 return 0;
