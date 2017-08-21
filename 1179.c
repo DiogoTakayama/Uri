c269d35e081355e76025b7817a16ca4339865a35
DiogoTakayama <diogo.takayamas@gmail.com>
2017-08-21 20:18:54 -0300

    Update 1179.c

--- 1179.c
+++ 1179.c
@@ -1,44 +1,61 @@
-
-//Não está 100%
+//Falta complementar
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    int par[5],impar[5];int QuantPar = 0,QuantImp = 0,x,i,j;
-    for(i=0;i<15;i++){
-            scanf("%d",&x);
-        if (x%2==0){
+    int par[5],impar[5];
+    int QuantPar = 0,QuantImp = 0;
+    int x,i,j,k,l;
+
+
+    for(i=0; i<15; i++)
+    {
+
+        scanf("%d",&x);
+        if (x%2==0)
+        {
             par [QuantPar]=x;
             QuantPar++;
+
+            if (QuantPar==5)
+            {
+
+                for (j=0; j<5; j++)
+                {
+                    printf("par[%d] = %d\n",j,par[j]);
+                    QuantPar =0;
+
+
+                }
+            }
         }
-        if (QuantPar==5){
-
-        for (j=0;j<5;j++)
-            printf("par[%d] = %d\n",j,par[j]);
-            QuantPar =0;
-
-
-        }else{
-
+        else
+        {
             impar [QuantImp]=x;
             QuantImp++;
-        if (QuantImp==5){
+            if (QuantImp==5)
+            {
+                for (k=0; k<5; k++)
+                {
+                    printf("impar[%d] = %d\n",k,impar[k]);
+                    QuantPar =0;
 
-        for (j=0;j<5;j++)
-            printf("impar[%d] = %d\n",j,impar[j]);
-            QuantImp =0;
+                }
+
+            }
+
         }
-        }
-     for (j=0;j<QuantImp;j++){
-            printf("impar[%d] = %d\n",j,impar[j]);
-            QuantImp =0;
-     }
-     for (j=0;j<QuantPar;j++){
-            printf("par[%d] = %d\n",j,par[j]);
-            QuantImp =0;
-     }
+
+
     }
+
+
+
+
+
+
+
 
     return 0;
 }
