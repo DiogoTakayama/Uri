d2ba3bea93d253b3cdb850a0fefe464efff7eb31
DiogoTakayama <diogo.takayamas@gmail.com>
2017-08-27 23:26:19 -0300

    Create 1743.c

---
+++ 1743.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int n1,n2,n3,n4,n5;
+int N1,N2,N3,N4,N5;
+scanf("%d%d%d%d%d",&n1,&n2,&n3,&n4,&n5);
+scanf("%d%d%d%d%d",&N1,&N2,&N3,&N4,&N5);
+
+if (n1==N1||n2==N2||n3==N3||n4==N4||n5==N5){
+
+    printf("N\n");
+}
+else
+    printf("Y\n");
+
+    return 0;
+}
