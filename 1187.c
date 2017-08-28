bd9720c795c885cb7d6294c1e93502fde990dd38
DiogoTakayama <diogo.takayamas@gmail.com>
2017-08-28 19:53:46 -0300

    Create 1187.c

---
+++ 1187.c
@@ -0,0 +1,28 @@
+  #include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,j;
+    double M[12][12];
+    char Letra[2];
+    double soma=0.0;
+    scanf("%s",&Letra);
+
+    for(i=0; i<12; i++)
+        for(j=0; j<12; j++)
+
+            scanf("%lf",&M[i][j]);
+
+    for(i=0; i<=4; i++)
+        for(j=i+1; j<=10-i; j++)
+        soma+=M[i][j];
+
+    if (Letra[0]=='M')
+        soma=soma/30.0;
+    printf("%.1lf\n",soma);
+
+
+    return 0;
+
+}
